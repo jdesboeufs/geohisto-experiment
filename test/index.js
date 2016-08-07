13d1c26095940c30cc28b65cc03988e16d7b172f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-08-07 14:25:59 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const geohisto = require('../');
 const test = require('tape');
 
 test('towns', t => {
-  t.plan(11);
+  t.plan(12);
   const towns = geohisto('towns');
 
   t.equal(towns.at('2000-01-01').get('46201').name, 'Montcuq');
@@ -18,8 +18,13 @@ test('towns', t => {
   t.equal(montcuq.successor.isActual(), true);
   t.equal(montcuq.getActual().name, 'Montcuq-en-Quercy-Blanc');
 
-  t.equal(towns.at('1943-01-01').exists('37261'), true);
-  t.equal(towns.at('1943-01-01').get('37261').name, 'Tours');
+  t.equal(towns.get('46025').name, 'Belmontet');
+  t.equal(towns.get('46025').toJSON().endDate, '2015-12-31');
+
+  t.equal(towns.at('now').get('46025'), null);
+
+  // t.equal(towns.at('1943-01-01').exists('37261'), true);
+  // t.equal(towns.at('1943-01-01').get('37261').name, 'Tours');
 
   // t.equal(towns.at('2004-01-01').getAll().length, 36568 + 45); // Towns + 45 municipal arrondissements
 });
